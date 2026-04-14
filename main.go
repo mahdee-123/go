2602a615f0d7e250d8cf1b597d45f062fcbd37f2
arnob <arnobmahdee@gmail.com>
2026-04-15 03:36:55 +0600

    unknown commit

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
-package main 
-
+package main
 
 import (
 	"fmt"
 	"my-projects/utils"
 )
 
-
-
 // var firstName string = "mahdee"
 // var lastName string = "hossain"
 
-var firstName, lastName = "mahdee",
+var firstName, lastName = "mahdee", "arnob"
 
 func main() {
+
 	utils.PrintSub()
 	utils.PrintSum()
 	fmt.Println(firstName)
