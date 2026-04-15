931898313c788a87871d520e95ac60f0edcd281d
mahdee-123 <arnobdev10@gmail.com>
2026-04-16 02:21:35 +0600

    updated

--- go-one/main.go
+++ go-one/main.go
@@ -14,13 +14,14 @@ var name2, age2, isCool2 = "arnob", 21, false
 // infer data type
 var id = 1110 
 
-// constants
-const pi  = 3.14
+// constants	
+const pi float32  = 3.14
 const appName string = "pussho"
 func main() {
 	
 	sum := utils.Add(23,4)
-	fmt.Println(name, age, isCool, name2, age2, isCool2,id)
+	fmt.Println(name, age, isCool, name2, age2, isCool2,id,pi,appName)
 	fmt.Println(sum)
+	printSomething(sum)
 	fmt.Println("hello world")
 }
